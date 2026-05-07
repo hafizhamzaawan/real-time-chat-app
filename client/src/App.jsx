35137c46b258b29fb8cd8ceb41d551e21b6d4945
Hamza <financeworldinfo9596@gmail.com>
2026-05-07 12:51:12 +0500

    fix: background image path for production

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,12 +4,14 @@ import HomePage from './pages/HomePage'
 import Loginpage from './pages/Loginpage'
 import ProfilePage from './pages/ProfilePage'
 import { useAuthContext } from './context/AuthContext'
+import bgImage from './assets/bgImage.svg'
 
 const App = () => {
   const { authUser } = useAuthContext();
 
   return (
-    <div className="bg-[url('./src/assets/bgImage.svg')] bg-cover bg-center bg-no-repeat">
+    <div style={{ backgroundImage: `url(${bgImage})` }}
+      className="bg-cover bg-center bg-no-repeat min-h-screen">
       <Routes>
         <Route path='/' element={authUser ? <HomePage /> : <Navigate to="/login" />} />
         <Route path='/login' element={!authUser ? <Loginpage /> : <Navigate to="/" />} />
